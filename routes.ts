17341b9c91722682d73ac6166e6aa906ba0038c7
unknown <edsnascimento89@gmail.com>
2021-09-05 19:29:38 -0300

    create new_routes/new_migrations/test

--- routes.ts
+++ routes.ts
@@ -1,10 +1,16 @@
 import {Router} from "express";
+import { SurveysController } from "./src/controllers/SurveysController";
 import {UserController} from "./src/controllers/UserController"
 
 const router = Router();
 
 const userControler = new UserController();
+const surveyControler = new SurveysController();
 
 router.post("/users", userControler.create);
+
+router.post("/surveys", surveyControler.create);
+router.get("/surveys", surveyControler.show);
+
 
 export { router };
