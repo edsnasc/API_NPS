805fdd4fde82bd48373997207149c58d49eee700
unknown <edsnascimento89@gmail.com>
2021-09-20 23:05:33 -0300

    bulding NpsController

--- routes.ts
+++ routes.ts
@@ -1,5 +1,6 @@
 import {Router} from "express";
 import { AnswerController } from "./src/controllers/AnswerController";
+import { NpsController } from "./src/controllers/NpsController";
 import { SendMailController } from "./src/controllers/SendMailController";
 import { SurveysController } from "./src/controllers/SurveysController";
 import {UserController} from "./src/controllers/UserController"
@@ -13,6 +14,8 @@ const sendMailController = new SendMailController();
 
 const answerController = new AnswerController();
 
+const npsController = new NpsController();
+
 router.post("/users", userControler.create);
 
 router.post("/surveys", surveyControler.create);
@@ -21,5 +24,7 @@ router.get("/surveys", surveyControler.show);
 router.post("/sendMail", sendMailController.execute);
 
 router.get("/answers/:value", answerController.execute);
+
+router.get("/nps/:survey_id", npsController.execute);
 
 export { router };
