bc53f73924286c768100985838c57eae990535fd
unknown <edsnascimento89@gmail.com>
2021-09-22 22:46:34 -0300

    bulding / update

--- src/controllers/NpsController.ts
+++ src/controllers/NpsController.ts
@@ -27,7 +27,9 @@ class NpsController {
 
         const totalAnswers = surveysUsers.length;
 
-        const calculate = ((promoters - detractor) / totalAnswers) * 100;
+        const calculate = Number(
+            (((promoters - detractor) / totalAnswers) * 100).toFixed(2)
+            );
 
         return response.json({
             detractor,
