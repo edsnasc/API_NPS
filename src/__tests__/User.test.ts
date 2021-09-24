3eadd1b222a11a4dcecafbb1baed60ef11417cb3
unknown <edsnascimento89@gmail.com>
2021-09-24 00:03:58 -0300

    refactoring code

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -2,7 +2,7 @@ import request from "supertest"
 import { getConnection } from "typeorm";
 import { app } from "../app"
 
-import createConnection from '../database'
+import createConnection from "../database"
 
 describe("Users", () => {
     beforeAll(async () => {
@@ -14,23 +14,23 @@ describe("Users", () => {
         const connection = getConnection();
         await connection.dropDatabase();
         await connection.close();
-    })
+    });
 
     it("Should be able to create a new user", async () => {
         const response = await request(app).post("/users")
         .send({
-            name: "User Exemple",
-            email: "user@example.com"
+            email: "user@example.com",
+            name: "User Exemple"
+            
         });
 
         expect(response.status).toBe(201);
     });
 
     it("Should not be able to create a user with exists email", async () => {
-        const response = await request(app).post("/users")
-        .send({
-            name: "User Exemple",
-            email: "user@example.com"
+        const response = await request(app).post("/users").send({          
+            email: "user@example.com",
+            name: "User Exemple"
         });
 
         expect(response.status).toBe(400);
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,6 +2,7 @@ import {Request, Response} from "express";
 import {getCustomRepository, getRepository } from "typeorm";
 import { UsersRepository } from "../repositories/UsersRepository";
 import * as yup from 'yup'
+import { AppError } from "../errors/AppErrors";
 
 
 class UserController {
@@ -20,7 +21,7 @@ class UserController {
         try {
             await schema.validate(request.body, { abortEarly: false});
         } catch (err) {
-            return response.status(400).json({error: err });
+            throw new AppError(err);
         }
 
         const usersRepository = getCustomRepository(UsersRepository);
@@ -31,9 +32,7 @@ class UserController {
         })
 
         if (userAlreadyExists) {
-            return response.status(400).json({
-                error: "User already exists!", 
-            });
+            throw new AppError("User already exists!");
         }
 
         const user = usersRepository.create({
